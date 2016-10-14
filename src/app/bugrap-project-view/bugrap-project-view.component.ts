6afaf05008bc9a257a039d02bb4bd412eecffed0
Viktor Lukashov <viktor@vaadin.com>
2016-10-14 14:19:14 +0300

    adds support for filtering tickets based on the assigned_to and status
    - the grid contents in the <bugrap-project-view> are updated dynamically when the assigned_to or status filters change
    - adds a binding for the Assignees and Status button groups

--- src/app/bugrap-project-view/bugrap-project-view.component.ts
+++ src/app/bugrap-project-view/bugrap-project-view.component.ts
@@ -15,26 +15,57 @@ export class BugrapProjectViewComponent implements OnInit, AfterViewInit, OnChan
   @Input() project: string;
   @Output('selected-tickets-changed') selectedTicketsChanged: EventEmitter<any> = new EventEmitter();
   @ViewChild('grid') grid: any;
+  @ViewChild('statusMenu') statusMenu: any;
 
   ALL_VERSIONS = 'All versions';
+
+  ASSIGNED_TO_ALL = 'all';
+  ASSIGNED_TO_ME = 'me';
+
+  STATUS_ALL = 'all';
+  STATUS_OPEN = 'open';
+  STATUS_CUSTOM = 'custom';
 
   STATUS_CHOICES = BugrapTicketStatus.getValueLabelPairs();
 
   lastSelectedVersions: Map<string, string> = new Map();
   versions: string[];
   version: string;
+  assignedToFilter: string = this.ASSIGNED_TO_ALL;
+  statusFilter: string = this.STATUS_ALL;
+  customStatusFilter: BugrapTicketStatus[] = [];
   tickets: BugrapTicket[];
   selectedTickets: BugrapTicket[] = [];
 
   constructor(private backend: BugrapBackendService) {}
+
+  // execute some callback async when the grid control is ready
+  execOnGrid(callback: (grid: any) => void) {
+    this.grid.nativeElement.then(() => callback(this.grid.nativeElement));
+  }
 
   ngOnInit() {
     this.tickets = this.backend.getTickets();
   }
 
   ngAfterViewInit() {
-    this.grid.nativeElement.then(() => {
-      this.gridReady(this.grid.nativeElement);
+    // initialize the grid control
+    this.execOnGrid((grid) => {
+      grid.columns[2].renderer = (cell: any) => {
+        cell.element.innerHTML = BugrapTicketType[cell.data];
+      };
+
+      let dateRenderer = (cell: any) => {
+        let now = moment();
+        let date = moment(cell.data);
+        cell.element.innerHTML = date.isValid() ? date.from(now) : '';
+      };
+
+      grid.columns[5].renderer = dateRenderer;
+      grid.columns[6].renderer = dateRenderer;
+
+      grid.addEventListener('selected-items-changed', () => this.onSelectionChange(grid));
+      grid.items = this.getGridItems.bind(this);
     });
   }
 
@@ -50,50 +81,64 @@ export class BugrapProjectViewComponent implements OnInit, AfterViewInit, OnChan
         this.lastSelectedVersions[change.previousValue] = this.version;
         this.version = lastSelectedVersion ? lastSelectedVersion : this.versions[0];
 
-        this.grid.nativeElement.then(() => this.grid.nativeElement.refreshItems());
+        this.onFiltersChanged();
       } else {
         this.version = this.versions[0];
       }
     }
   }
 
-  gridReady(gridElem: any) {
-    gridElem.items = this.getGridItems.bind(this);
-
-    gridElem.columns[2].renderer = (cell: any) => {
-      cell.element.innerHTML = BugrapTicketType[cell.data];
-    };
-
-    let dateRenderer = (cell: any) => {
-      let now = moment();
-      let date = moment(cell.data);
-      cell.element.innerHTML = date.isValid() ? date.from(now) : '';
-    };
-
-    gridElem.columns[5].renderer = dateRenderer;
-    gridElem.columns[6].renderer = dateRenderer;
-
-    gridElem.addEventListener('selected-items-changed', () => this.onSelectionChange(gridElem));
+  // Handles updates to the current version:
+  // - toggles the version column visibility
+  // - handles filters change
+  onVersionChanged($event: any) {
+    let hidden = $event.target.value != this.ALL_VERSIONS;
+    this.execOnGrid(grid => grid.columns[0].hidden = hidden);
+    this.onFiltersChanged();
   }
 
+  // Handles updates to the custom status filter multi-select
+  // - iterates over the list of checkboxes and updates the customStatusFilter array property accordingly
+  // - handles filters change
+  onCustomStatusFilterChanged() {
+    let selected = this.statusMenu.nativeElement.selectedItems;
+    this.customStatusFilter = selected.map(checkbox => checkbox.value);
+
+    this.onFiltersChanged();
+  }
+
+  // Handles updates to any of the filter properties
+  onFiltersChanged() {
+    // A call to .refreshItem() triggers a call to the .getGridItems() callback.
+    // The .getGridItem() applies the current filters and returns filtered data.
+    this.execOnGrid(grid => grid.refreshItems());
+  }
+
+  // the callback for <vaadin-grid> to fetch contents dynamically
   getGridItems(params, callback) {
+    // NOTE: There is a potential for optimization here if performance starts be an issue
+    // 1. do the filtering on the backend to save both on client CPU utilization and on traffic
+    // 2. do the slicing together with filtering to save even more on CPU and traffic
+
+    // Find all tickets that match the current filters for project, version, assigned_to and status
     let filtered = this.tickets.filter(ticket => {
       let projectMatch = ticket.project == this.project;
       let versionMatch = this.version == this.ALL_VERSIONS || ticket.version == this.version;
-      return projectMatch && versionMatch;
+      let assignedToMatch = this.assignedToFilter == this.ASSIGNED_TO_ALL ||
+        ticket.assigned_to == this.backend.getCurrentUser().name;
+      let statusMatch = this.statusFilter == this.STATUS_ALL ||
+        (this.statusFilter == this.STATUS_OPEN && ticket.status == BugrapTicketStatus.Open) ||
+        (this.statusFilter == this.STATUS_CUSTOM && this.customStatusFilter.indexOf(ticket.status) > -1);
+      return projectMatch && versionMatch && assignedToMatch && statusMatch;
     });
-    callback(filtered, filtered.length);
+
+    // Then extract a slice of the result set and return only the requested number of items.
+    let slice = filtered.slice(params.index, params.index + params.count);
+    callback(slice, filtered.length);
   }
 
-  onVersionChanged($event: any) {
-    let hidden = $event.target.value != this.ALL_VERSIONS;
-    let gridElem = this.grid.nativeElement;
-    gridElem.then(() => {
-      gridElem.columns[0].hidden = hidden;
-      gridElem.refreshItems();
-    });
-  }
-
+  // Handles changes in the grid tickets selection
+  // - gathers the IDs of the selected tickets and fires the 'selected-tickets-changed' event
   onSelectionChange(grid: any) {
     let selected = grid.selection.selected();
     let selectedTickets = this.tickets.filter((_, index) => {
@@ -103,11 +148,12 @@ export class BugrapProjectViewComponent implements OnInit, AfterViewInit, OnChan
   }
 
   refreshTickets() {
-    let gridElem = this.grid.nativeElement;
-    let selected = gridElem.selection.selected().slice(0);
+    let selected = this.grid.nativeElement.selection.selected().slice(0);
     this.tickets = this.backend.getTickets();
-    gridElem.then(() => {
-      selected.forEach(selectedItemIndex => gridElem.selection.select(selectedItemIndex));
+
+    this.execOnGrid(grid => grid.refreshItems());
+    this.execOnGrid(grid => {
+      selected.forEach(selectedItemIndex => grid.selection.select(selectedItemIndex));
     });
   }
 }
